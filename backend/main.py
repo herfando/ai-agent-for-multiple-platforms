c424d08e27b8fe85c41c90a7ca73591f45995aa4
Herfando <herfando_91@yahoo.com>
2026-06-01 18:27:49 +0700

    modified:   main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 
 from app.api.auth import router as auth_router
+from app.api.messages import router as message_router
 
 app = FastAPI()
 
 # ROUTES
 app.include_router(auth_router, prefix="/auth")
+
+app.include_router(
+    message_router,
+    prefix="/messages",
+    tags=["Messages"]
+)
 
 # ROOT
 @app.get("/")
@@ -15,3 +22,5 @@ def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+    
